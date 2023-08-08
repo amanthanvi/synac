dc06a1d629d2cbc881b8127193a9f9f7e82430d2
Aman Thanvi <aman_thanvi@outlook.com>
2023-08-08 05:19:32 -0400

    Update import.js

--- backend/import.js
+++ backend/import.js
@@ -3,39 +3,41 @@ const csv = require("csv-parser");
 const mongoose = require("mongoose");
 const Term = require("./models/Term");
 const dotenv = require("dotenv");
-const path = require("path"); // Added path module
+const path = require("path");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 
-function importTerms() {
-  mongoose.connect(uri, {
+async function importTerms() {
+  await mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
   const connection = mongoose.connection;
 
-  connection.once("open", function () {
+  connection.once("open", async function () {
     console.log("MongoDB database connection established successfully");
 
-    // Updated the path to the CSV file
+    const terms = [];
+
     fs.createReadStream(path.join(__dirname, 'terms.csv'))
       .pipe(csv())
       .on("data", (row) => {
-        const term = new Term({
+        terms.push({
           term: row.Term,
           definition: row.Definition,
-          source: row.Source,
+          // Removed the source field
         });
-
-        term
-          .save()
-          .then(() => console.log("Term saved!"))
-          .catch((error) => console.error(error));
       })
-      .on("end", () => {
-        console.log("CSV file successfully processed");
-        connection.close(); // Close the connection after processing
+      .on("end", async () => {
+        try {
+          await Term.insertMany(terms);
+          console.log("CSV file successfully processed");
+        } catch (error) {
+          console.error(error);
+        } finally {
+          connection.close(); // Close the connection after processing
+        }
       });
   });
 }
