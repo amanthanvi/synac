df81dde7c951c6addd2152293a1e1bb6f059aed8
Aman Thanvi <aman_thanvi@outlook.com>
2023-08-08 03:25:05 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost/synac", {
+// Use the connection string from an environment variable
+const uri = process.env.MONGODB_URI || "mongodb://localhost/synac"; // Fallback to localhost if no environment variable is set
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-const connection = mongoose.connection;
-
-connection.once("open", function () {
-  console.log("MongoDB database connection established successfully");
-});
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
 const termsRouter = require("./routes/terms");
 const suggestionsRouter = require("./routes/suggestions");
@@ -24,6 +22,6 @@ const suggestionsRouter = require("./routes/suggestions");
 app.use("/suggestions", suggestionsRouter);
 app.use("/terms", termsRouter);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => { // Use Heroku's assigned port if available
   console.log("Server is running on Port: 5000");
 });
