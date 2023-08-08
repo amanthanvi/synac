a7a0c15a577998a2825b71b26d583bdb8ae634df
Aman Thanvi <aman_thanvi@outlook.com>
2023-08-08 05:13:12 -0400

    Update Term.js

--- backend/models/Term.js
+++ backend/models/Term.js
@@ -4,7 +4,7 @@ const termSchema = new mongoose.Schema(
   {
     term: { type: String, required: true },
     definition: { type: String, required: true },
-    category: { type: String, required: true },
+    category: { type: String, required: false }, // Changed from true to false
   },
   {
     timestamps: true,
