10bdd41a671a60a90aa0a7182da76e165a3c5a51
Aman Thanvi <aman_thanvi@outlook.com>
2023-08-28 00:03:25 -0400

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,58 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
-const importTerms = require('./import');
+const importTerms = require("./import");
 
-// Middleware
-app.use(express.json());
+const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(cors());
+app.use(express.json());
 
-// MongoDB connection
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-mongoose.connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-// Import and use routes
+app.use(express.static("public"));
+
 const termsRouter = require("./routes/terms");
 const suggestionsRouter = require("./routes/suggestions");
 
-app.use("/suggestions", suggestionsRouter);
 app.use("/terms", termsRouter);
+app.use("/suggestions", suggestionsRouter);
 
-// Serve static files from the frontend directory
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// The "catchall" handler: for any request that doesn't match one above, send back the index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+// Error Handling (as from your previous server.js)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on Port: ${PORT}`);
-});
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("Successfully connected to MongoDB!");
+
+    app.listen(port, () => {
+      console.log(`Server is running on Port: ${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+}
+
+// Start the server
+startServer();
 
 // Schedule the importTerms function to run every 24 hours
 const importInterval = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
