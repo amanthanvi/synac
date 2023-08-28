ea78523ef5eac2879f0f613104e978d0702588ec
Aman Thanvi <aman_thanvi@outlook.com>
2023-08-27 22:39:58 -0400

    Updated routes files

--- backend/routes/suggestions.js
+++ backend/routes/suggestions.js
@@ -1,22 +1,37 @@
 const router = require("express").Router();
-let Suggestion = require("../models/Suggestion");
+const Suggestion = require("../models/Suggestion");
 
+// Utility function for sending errors
+const sendError = (res, err) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal server error" });
+};
+
+// Get all suggestions
 router.route("/").get((req, res) => {
   Suggestion.find()
-    .then((suggestions) => res.json(suggestions))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then((suggestions) => res.json({ data: suggestions }))
+    .catch((err) => sendError(res, err));
 });
 
+// Add a suggestion
 router.route("/add").post((req, res) => {
   const term = req.body.term;
   const definition = req.body.definition;
-
-  const newSuggestion = new Suggestion({ term, definition });
+  const category = req.body.category;
+  const newSuggestion = new Suggestion({ term, definition, category });
 
   newSuggestion
     .save()
-    .then(() => res.json("Suggestion added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then(() => res.json({ data: "Suggestion added!" }))
+    .catch((err) => sendError(res, err));
+});
+
+// Delete a suggestion
+router.route("/:id").delete((req, res) => {
+  Suggestion.findByIdAndDelete(req.params.id)
+    .then(() => res.json({ data: "Suggestion deleted." }))
+    .catch((err) => sendError(res, err));
 });
 
 module.exports = router;
--- backend/routes/terms.js
+++ backend/routes/terms.js
@@ -1,51 +1,61 @@
 const router = require("express").Router();
-let Term = require("../models/Term");
+const Term = require("../models/Term");
 
+// Utility function for sending errors
+const sendError = (res, err) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal server error" });
+};
+
+// Get all terms
 router.route("/").get((req, res) => {
   Term.find()
-    .then((terms) => res.json(terms))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then((terms) => res.json({ data: terms }))
+    .catch((err) => sendError(res, err));
 });
 
+// Add a term
 router.route("/add").post((req, res) => {
   const term = req.body.term;
   const definition = req.body.definition;
-  const category = req.body.category; // new category field
-
+  const category = req.body.category;
   const newTerm = new Term({ term, definition, category });
 
   newTerm
     .save()
-    .then(() => res.json("Term added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then(() => res.json({ data: "Term added!" }))
+    .catch((err) => sendError(res, err));
 });
 
+// Delete a term
 router.route("/:id").delete((req, res) => {
   Term.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Term deleted."))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then(() => res.json({ data: "Term deleted." }))
+    .catch((err) => sendError(res, err));
 });
 
-// New search endpoint
-router.route("/search/:query").get((req, res) => {
-  const regex = new RegExp(req.params.query, "i"); // i for case insensitive
+// Search for a term
+router.route("/search").get((req, res) => {
+  const searchTerm = new RegExp(req.query.term, "i"); // Making the search case-insensitive
+  Term.find({ term: searchTerm })
+    .then((terms) => res.json({ data: terms }))
+    .catch((err) => sendError(res, err));
+});
+
+// Get terms starting with a specific letter
+router.route("/alphabet/:letter").get((req, res) => {
+  const letter = req.params.letter;
+  const regex = new RegExp("^" + letter, "i"); // Using '^' to match the start of the string
   Term.find({ term: regex })
-    .then((terms) => res.json(terms))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then((terms) => res.json({ data: terms }))
+    .catch((err) => sendError(res, err));
 });
 
-// basic alphabetical filter
-router.route("/starts-with/:letter").get((req, res) => {
-  let regex = new RegExp("^" + req.params.letter, "i");
-  Term.find({ term: regex })
-    .then((terms) => res.json(terms))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
+// Get a list of all unique categories and the count of terms in each
 router.route("/categories").get((req, res) => {
   Term.aggregate([{ $group: { _id: "$category", count: { $sum: 1 } } }])
-    .then((categories) => res.json(categories))
-    .catch((err) => res.status(400).json("Error: " + err));
+    .then((categories) => res.json({ data: categories }))
+    .catch((err) => sendError(res, err));
 });
 
 module.exports = router;
