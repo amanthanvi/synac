4ddbd76345d9875c895f8bb25911b809c53d27fc
amanthanvi <aman_thanvi@outlook.com>
2026-01-02 12:27:54 -0500

    chore: bootstrap pnpm workspace

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: ['**/dist/**', '**/.next/**', '**/node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      'no-console': 'off',
+    },
+  },
+];
